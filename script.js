99b649446d9b484f3ac6aae834aeccaef7ceafbd
Jorge <73******@continental.edu.pe>
2025-03-29 19:51:50 -0500

    Jorge Added & DOOM.exe on the menu added

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
         member.style.animationDelay = `${index * 0.2}s`;
     });
 });
+
+var dosbox = new Dosbox({
+    id: "dosbox",
+    onload: function (dosbox) {   dosbox.run("https://js-dos.com/cdn/upload/DOOM-@evilution.zip", "./DOOM/DOOM.EXE");
+    },
+    onrun: function (dosbox, app) {
+      console.log("App '" + app + "' is runned");
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const members = document.querySelectorAll(".member");
+
+    members.forEach((member, index) => {
+        member.style.animationDelay = `${index * 0.2}s`;
+    });
+});
+
